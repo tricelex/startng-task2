4cea984f4dd2e660871fee6c8875f42035577969
Chuckz Okoye <tricelex@gmail.com>
2020-04-08 01:52:04 +0100

    Initial Commit containing startup files

---
+++ user_validate.py
@@ -0,0 +1,9 @@
+
+first_name = input("Enter your first name: ")
+print(first_name)
+
+last_name = input("Enter your last name: ")
+print(last_name)
+
+email = input("Enter your email: ")
+print(email)
